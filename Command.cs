e1b4bbcd45554275fc9ddefb89657930800353da
TRevit <trthomas16@gmail.com>
2024-04-11 16:17:34 -0400

    testing

--- Command.cs
+++ Command.cs
@@ -21,6 +21,12 @@ namespace TagLinkedRooms
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document doc = uidoc.Document;
 
+            
+            List<Room> m_rooms = new List<Room>();                   //a list to store all rooms in the project
+            List<RoomTag> m_roomTags = new List<RoomTag>();          //a list to store all room tags in the project
+            List<Room> m_roomsWithTag = new List<Room>();            //a list to store all rooms with tag
+            List<Room> m_roomsWithoutTag = new List<Room>();         //a list to store all rooms without tag
+           
             // Get all linked models
             var revitLinks = new FilteredElementCollector(doc)
                 .OfClass(typeof(RevitLinkInstance))
@@ -43,34 +49,49 @@ namespace TagLinkedRooms
                 return Result.Failed;
             }
 
+
             // Retrieve rooms from the linked model document
-            var linkedRooms = new FilteredElementCollector(linkDoc)
-                .OfCategory(BuiltInCategory.OST_Rooms)
-                .WhereElementIsNotElementType()
-                .Cast<Room>()
-                .ToList();
+         
+
+
+            ElementFilter filterRoom = new ElementClassFilter(typeof(SpatialElement));
+            FilteredElementCollector roomCollector = new FilteredElementCollector(uiapp.ActiveUIDocument.Document);
+            roomCollector.WherePasses(filterRoom);
+
+            foreach (Autodesk.Revit.DB.Element ee in roomCollector)
+            {
+                Room tmpRoom = ee as Room;
+                if (null != tmpRoom)
+                {
+                    m_rooms.Add(tmpRoom);
+                    continue;
+                }
+            }
+
 
             // Start transaction for placing room tags
             using (Transaction transaction = new Transaction(doc, "Place Room Tags"))
             {
                 transaction.Start();
 
-                // Iterate over the linked rooms
-                foreach (Room room in linkedRooms)
+                foreach (Room tmpRoom in m_roomsWithoutTag)
                 {
-                    // Get the room location point
-                    LocationPoint roomLocation = room.Location as LocationPoint;
-                    if (null != LocationPoint)
+                    //get the location point of the room
+                    LocationPoint locPoint = tmpRoom.Location as LocationPoint;
+                    if (null != locPoint)
                     {
-                        // Convert XYZ to UV
-                        double u = roomLocation
-                        UV tagLocation = new UV(roomLocation.X, roomLocation.Y);
+                        //create a instance of UV class
+                        double u = locPoint.Point.X;
+                        double v = locPoint.Point.Y;
 
-                        // Place new room tag
-                        uiapp.ActiveUIDocument.Document.Create.NewRoomTag(new LinkElementId(room.Id), tagLocation, uiapp.ActiveUIDocument.ActiveView.Id);
-                      
+                        UV point = uiapp.Application.Create.NewUV(u, v);
+
+                        //create room tag
+
+                        uiapp.ActiveUIDocument.Document.Create.NewRoomTag(new LinkElementId(tmpRoom.Id), point, null);
                     }
                 }
+            }
 
                 // Commit transaction for placing room tags
                 transaction.Commit();
@@ -78,6 +99,7 @@ namespace TagLinkedRooms
             }
 
             return Result.Succeeded;
+            
         }
     }
 }
